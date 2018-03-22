c2aab90def530b1d6237250b295d4613cbd8b763
Joel Aro <joelaro.to@gmail.com>
2018-03-22 12:19:24 -0400

    Removing unecessary error outputs

--- viscosity_helper.sh
+++ viscosity_helper.sh
@@ -10,6 +10,7 @@ BASE_URL='https://www.sparklabs.com'
 DL_PATH='/downloads/Viscosity.dmg'
 DMG='Viscosity.dmg'
 MOUNT_POINT='/Volumes/Viscosity'
+CON_CONFIG='config.conf'
 
 # Kill all processes related to Viscosity ie. ViscosityHelper. This is so that
 # the files can easily be removed for a clean install
@@ -27,6 +28,7 @@ remove_path() {
 	for p in $@
 	do
 		rm -rf $p
+	echo "Deleted $p"
 	done
 }
 
@@ -43,7 +45,7 @@ wipe_viscosity() {
 	VHELP_PLIST="/Library/LaunchDaemons/com.sparklabs.ViscosityHelper.plist"
 	if [ -f $VHELP_PLIST ]
 	then
-		launchctl unload $VHELP_PLIST > /dev/null
+		launchctl unload $VHELP_PLIST 2> /dev/null
 	fi
 	
 	# These files/directories need to be checked first if they exist!
@@ -59,11 +61,18 @@ then
 	exit 1
 fi
 
+if [[ ! -f $CON_CONFIG ]]
+then
+	echo "Config file not found. Please download the ovpn-config .zip file from "\
+	"login.mozilla.com and copy the config.conf file to the script's directory."
+	exit 1
+fi
+
 # User will run the script with the intention to have a fresh install
 wipe_viscosity
 
 # Grab viscosity from Sparklabs website
-if [[ ! -f 'Viscosity.dmg' ]]
+if [[ ! -f $DMG ]]
 then
 	wget --no-check-certificate -q $BASE_URL$DL_PATH
 	if [[ $? -ne 0 ]]; then echo "Could not fetch .dmg file"; exit 1; fi
@@ -75,13 +84,13 @@ if [[ $? -ne 0 ]]; then echo "Encountered a problem while mounting .dmg"; exit 1
 
 cp -rf $MOUNT_POINT/Viscosity.app /Applications
 
+# Unmount
 hdiutil detach $MOUNT_POINT > /dev/null
-if [[ $? -ne 0 ]]
-then
-	echo "Encountered a problem while unmounting .dmg"
-	exit 1
-fi
+if [[ $? -ne 0 ]]; then echo "Encountered a problem while unmounting .dmg"; exit 1; fi
 
 rm $DMG
+
+echo "Clean install completed!"
+echo "Starting up Viscosity..."
 
 /Applications/Viscosity.app/Contents/MacOS/Viscosity &
