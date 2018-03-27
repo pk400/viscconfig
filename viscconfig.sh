b2602cbf705fff8fcd4aeae28c80a4e4ac068a27
Joel Aro <joelaro.to@gmail.com>
2018-03-27 02:16:33 -0400

    Able to import config.conf
    Need to add login.mozilla.com credentials somehow...

--- viscconfig.sh
+++ viscconfig.sh
@@ -10,7 +10,13 @@ BASE_URL='https://www.sparklabs.com'
 DL_PATH='/downloads/Viscosity.dmg'
 DMG='Viscosity.dmg'
 MOUNT_POINT='/Volumes/Viscosity'
-PKG='Viscosity Installer.pkg'
+PKG='moz_config.pkg'
+ALL_FILES=(
+	/Library/PrivilegedHelperTools/com.sparklabs.ViscosityHelper
+	/Library/LaunchDaemons/com.sparklabs.ViscosityHelper.plist
+	/Library/Application\ Support/Viscosity
+	~/Library/Preferences/com.viscosityvpn.Viscosity.plist
+)
 
 # Kill all processes related to Viscosity ie. ViscosityHelper. This is so that
 # the files can easily be removed for a clean install
@@ -18,42 +24,28 @@ kill_p() {
 	LISTP=$(ps -axco command | sort | uniq)
 	for p in $@
 	do
-		echo "$LISTP" | grep -w $p
 		ISRUNNING=$(echo "$LISTP" | grep -w $p | wc -l)
-		if [[ $ISRUNNING -gt 0 ]]; then killall $p; echo "Killing $p ..."; fi
+		if [[ $ISRUNNING -gt 0 ]]; then killall $p && echo "Killing $p ..."; fi
 	done
 }
 
 # Removes all related process files and their directories
-remove_path() {
-	for p in $@
-	do
-		rm -rf $p
-	echo "Deleted $p"
-	done
-}
+remove_path()
+	for p in "$@"; do [[ -f $p ]] && (rm -rf $p && echo "Deleting $p"); done
 
 # Closes all open instances of Viscosity and removes related files/directories
 # to ensure a fresh install
 wipe_viscosity() {
-	echo "Uninstalling Viscosity ..."
+	echo "Clearing previous Viscosity data ..."
 	kill_p Viscosity com.sparklabs.ViscosityHelper
 
-	defaults delete com.viscosityvpn.Viscosity 2> /dev/null
-	[[ $? = 0 ]] && echo "Viscosity reset defaults successful" \
-	|| echo "Viscosity reset defaults unsuccessful"
+	defaults delete com.viscosityvpn.Viscosity 2> /dev/null \
+	&& echo "Viscosity reset defaults successful"
 	
-	VHELP_PLIST="/Library/LaunchDaemons/com.sparklabs.ViscosityHelper.plist"
-	if [ -f $VHELP_PLIST ]
-	then
-		launchctl unload $VHELP_PLIST 2> /dev/null
-	fi
+	launchctl unload ${ALL_FILES[1]}
 	
 	# These files/directories need to be checked first if they exist!
-	remove_path "~/Library/PrivilegedHelperTools/com.sparklabs.ViscosityHelper" \
-	"~Library/LaunchDaemons/com.sparklabs.ViscosityHelper.plist" \
-	"~/Library/Application Support/Viscosity"\
-	"~/Library/Preferences/com.viscosityvpn.Viscosity.plist"
+	remove_path "${ALL_FILES[@]}"
 }
 
 # Ensures that all commands throughout the program have the proper admin privileges
@@ -63,11 +55,7 @@ then
 	exit 1
 fi
 
-if [[ ! -f $PKG ]]
-then
-	echo "Viscosity Installer.pkg not found."
-	exit 1
-fi
+[[ ! -f $PKG ]] && (echo "Viscosity Installer.pkg not found."; exit 1)
 
 # User will run the script with the intention to have a fresh install
 wipe_viscosity
@@ -77,23 +65,25 @@ if [ ! -f $DMG ]
 then
 	echo "Downloading Viscosity ..."
 	wget --no-check-certificate -q $BASE_URL$DL_PATH
-	if [[ $? -ne 0 ]]; then echo "Could not fetch .dmg file"; exit 1; fi
+	[[ $? -ne 0 ]] && (echo "Could not fetch .dmg file"; exit 1)
 fi
 
 # Mounts .dmg file
 echo "Installing Viscosity ..."
 hdiutil attach $DMG > /dev/null
-if [[ $? -ne 0 ]]; then echo "Encountered a problem while mounting .dmg"; exit 1; fi
+[[ $? -ne 0 ]] && (echo "Encountered a problem while mounting .dmg"; exit 1)
 
 cp -rf $MOUNT_POINT/Viscosity.app /Applications
 
 # Unmount
 hdiutil detach $MOUNT_POINT > /dev/null
-if [[ $? -ne 0 ]]; then echo "Encountered a problem while unmounting .dmg"; exit 1; fi
+[[ $? -ne 0 ]] && (echo "Encountered a problem while unmounting .dmg"; exit 1)
 
-#rm $DMG
+rm $DMG
 
 echo "Clean install completed!"
 
 echo "Configuring Mozilla settings ..."
 installer -pkg "$PKG" -target /
+
+open -a /Applications/Viscosity.app/Contents/MacOS/Viscosity
