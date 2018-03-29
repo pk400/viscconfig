4c2e1a22b8384e29921291152e0a9dc878281350
Joel Aro <joelaro.to@gmail.com>
2018-03-29 17:07:50 -0400

    Changed script to only installing a clean copy of Viscosity

--- viscconfig.sh
+++ viscconfig.sh
@@ -1,21 +1,13 @@
 #! /bin/bash
 
 # Author: Joel Aro
-# Description: This script installs/configures Viscosity to be used for 
-# a Mozilla employee.
+# Description: This script installs a fresh copy of Viscosity.
 
 
 # Declare some variables that need to be accessed from functions as well
-BASE_URL='https://www.sparklabs.com'
-DL_PATH='/downloads/Viscosity.dmg'
-DMG='Viscosity.dmg'
-MOUNT_POINT='/Volumes/Viscosity'
-PKG='moz_config.pkg'
 ALL_FILES=(
-	/Library/PrivilegedHelperTools/com.sparklabs.ViscosityHelper
-	/Library/LaunchDaemons/com.sparklabs.ViscosityHelper.plist
-	/Library/Application\ Support/Viscosity
 	~/Library/Preferences/com.viscosityvpn.Viscosity.plist
+	~/Library/Application\ Support/Viscosity/*
 )
 
 # Kill all processes related to Viscosity ie. ViscosityHelper. This is so that
@@ -25,13 +17,12 @@ kill_p() {
 	for p in $@
 	do
 		ISRUNNING=$(echo "$LISTP" | grep -w $p | wc -l)
-		if [[ $ISRUNNING -gt 0 ]]; then killall $p && echo "Killing $p ..."; fi
+		if [[ $ISRUNNING -gt 0 ]]; then killall $p || echo "Killing $p ..."; fi
 	done
 }
 
 # Removes all related process files and their directories
-remove_path()
-	for p in "$@"; do [[ -f $p ]] && (rm -rf $p && echo "Deleting $p"); done
+remove_path() for p in "$@"; do rm -rf "$p"; done
 
 # Closes all open instances of Viscosity and removes related files/directories
 # to ensure a fresh install
@@ -40,50 +31,24 @@ wipe_viscosity() {
 	kill_p Viscosity com.sparklabs.ViscosityHelper
 
 	defaults delete com.viscosityvpn.Viscosity 2> /dev/null \
-	&& echo "Viscosity reset defaults successful"
+	|| echo "Viscosity reset defaults successful"
 	
-	launchctl unload ${ALL_FILES[1]}
+	launchctl unload ${ALL_FILES[1]} 2> /dev/null
 	
 	# These files/directories need to be checked first if they exist!
 	remove_path "${ALL_FILES[@]}"
 }
 
 # Ensures that all commands throughout the program have the proper admin privileges
-if [[ $(id -u) -ne 0 ]]
-then
-	echo "Script needs admin privileges to run. Please try again with sudo."
-	exit 1
-fi
-
-[[ ! -f $PKG ]] && (echo "Viscosity Installer.pkg not found."; exit 1)
+[ $(id -u) -ne 0 ] && echo "Try again with sudo." && exit 1
 
 # User will run the script with the intention to have a fresh install
 wipe_viscosity
 
-# Grab viscosity from Sparklabs website
-if [ ! -f $DMG ]
-then
-	echo "Downloading Viscosity ..."
-	wget --no-check-certificate -q $BASE_URL$DL_PATH
-	[[ $? -ne 0 ]] && (echo "Could not fetch .dmg file"; exit 1)
-fi
+# Remove old Viscosity
+rm -rf /Applications/Viscosity.app/ 2> /dev/null
 
-# Mounts .dmg file
-echo "Installing Viscosity ..."
-hdiutil attach $DMG > /dev/null
-[[ $? -ne 0 ]] && (echo "Encountered a problem while mounting .dmg"; exit 1)
-
-cp -rf $MOUNT_POINT/Viscosity.app /Applications
-
-# Unmount
-hdiutil detach $MOUNT_POINT > /dev/null
-[[ $? -ne 0 ]] && (echo "Encountered a problem while unmounting .dmg"; exit 1)
-
-rm $DMG
+# Install new one
+cp -r Viscosity.app /Applications/ 2> /dev/null
 
 echo "Clean install completed!"
-
-echo "Configuring Mozilla settings ..."
-installer -pkg "$PKG" -target /
-
-open -a /Applications/Viscosity.app/Contents/MacOS/Viscosity
